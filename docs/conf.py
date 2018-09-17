cff113f1c20b035078abf13b49b1ad3ec01c23e7
bmorcos <morcos.ben@gmail.com>
2018-09-17 15:04:35 -0400

    Update theme, add logos

--- docs/conf.py
+++ docs/conf.py
@@ -8,9 +8,9 @@ import sys
 
 try:
     import nengo_fpga
-    import sphinx_rtd_theme
+    import nengo_sphinx_theme
 except ImportError:
-    print("To build the documentation, nengo_fpga and sphinx_rtd_theme "
+    print("To build the documentation, nengo_fpga and nengo_sphinx_theme "
           "must be installed in the current environment. Please install these "
           "and their requirements first. A virtualenv is recommended!")
     sys.exit(1)
@@ -31,7 +31,7 @@ numfig = True
 
 # -- sphinx.ext.autodoc
 autoclass_content = 'both'  # class and __init__ docstrings are concatenated
-autodoc_default_flags = ['members']
+autodoc_default_flags = {'members': None}
 autodoc_member_order = 'bysource'  # default is alphabetical
 
 # -- sphinx.ext.intersphinx
@@ -63,23 +63,20 @@ authors = u'Applied Brain Research'
 copyright = nengo_fpga.__copyright__
 version = '.'.join(nengo_fpga.__version__.split('.')[:2])  # Short X.Y version
 release = nengo_fpga.__version__  # Full version, with tags
-pygments_style = 'default'
+pygments_style = 'friendly'
 
 # -- Options for HTML output --------------------------------------------------
 
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'nengo_sphinx_theme'
 html_title = "Nengo FPGA {0} docs".format(release)
 html_static_path = ['_static']
-# html_favicon = os.path.join('_static', 'favicon.ico')
-# html_logo = os.path.join('_static', 'square-light.svg')
-html_context = {
-    'css_files': [os.path.join('_static', 'custom.css')],
-}
+html_favicon = os.path.join('_static', 'favicon.ico')
+html_logo = os.path.join('_static', 'full-light.svg')
 html_use_smartypants = True
 htmlhelp_basename = 'Nengodoc'
 html_last_updated_fmt = ''  # Suppress 'Last updated on:' timestamp
 html_show_sphinx = False
+html_sidebars = {"**": ["sidebar.html"]}
 
 # -- Options for LaTeX output -------------------------------------------------
 
